2553023bf4f1b422ad512b688148e49b0ee9d2a2
MaccBass <besta.k0424@gmail.com>
2023-06-19 03:18:09 +0900

    알림에 아이콘 추가

--- app/src/main/java/com/example/keepfresh/MainActivity.java
+++ app/src/main/java/com/example/keepfresh/MainActivity.java
@@ -1,6 +1,6 @@
 package com.example.keepfresh;
 
-import android.Manifest
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.content.DialogInterface;
